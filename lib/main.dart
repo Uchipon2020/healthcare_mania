df3ab5d6502fe3b61192e3e79f47fc51ecc13348
hu-mania <uchiyama.s@timeshappy.com>
2022-11-01 09:42:36 +0900

    ようやくアップデートができて、エラーが消えた。

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,10 @@ import 'package:healthcare_mania/screens/list_screen.dart';
 import 'package:healthcare_mania/screens/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -20,10 +18,10 @@ class MyApp extends StatelessWidget {
       ),
       restorationScopeId: LoginScreen.id,
       routes: {
-    DetailScreen.id:(context) => DetailScreen(),
-        ListScreen.id:(context) => ListScreen(),
-        LoginScreen.id:(context) => LoginScreen(),
-    },
+        DetailScreen.id: (context) => DetailScreen(),
+        ListScreen.id: (context) => ListScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+      },
     );
   }
 }
